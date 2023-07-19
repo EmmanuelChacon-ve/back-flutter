9ff74ca9f1c01ea791ae98300c36db5b171b4b9b
Emmanuel Chacon <emmanuelechra@gmail.com>
2023-07-19 04:08:20 -0400

    roles

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,54 @@
+DROP TABLE IF EXISTS roles CASCADE;
+CREATE TABLE roles(
+	id BIGSERIAL PRIMARY KEY,
+	name VARCHAR(180) NOT NULL UNIQUE,
+	image VARCHAR(255) NULL, 
+	route VARCHAR(255) NULL,
+	created_at TIMESTAMP(0) NOT NULL,
+	updated_at TIMESTAMP(0) NOT NULL
+);
+
+INSERT INTO roles (
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'CLIENTE',
+	'client/products/list',
+	'2023-07-12',
+	'2023-07-12'
+);
+
+
+INSERT INTO roles (
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'ORGANIZACION',
+	'org/orders/list',
+	'2023-07-12',
+	'2023-07-12'
+);
+
+INSERT INTO roles (
+	name,
+	route,
+	created_at,
+	updated_at
+)
+VALUES(
+	'ADMINISTRADOR',
+	'admin/orders/list',
+	'2023-07-12',
+	'2023-07-12'
+);
+
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
 	id BIGSERIAL PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
@@ -11,4 +62,15 @@ CREATE TABLE users(
 	notification_token VARCHAR(255) NULL,
 	created_at TIMESTAMP(0) NOT NULL,
 	updated_at TIMESTAMP(0) NOT NULL
+);
+
+DROP TABLE IF EXISTS user_has_roles CASCADE;
+CREATE TABLE user_has_roles(
+	id_user BIGSERIAL NOT NULL,
+	id_rol BIGSERIAL NOT NULL,
+	created_at TIMESTAMP(0) NOT NULL,
+	updated_at TIMESTAMP(0) NOT NULL,
+	FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(id_rol) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(id_user, id_rol)
 );
